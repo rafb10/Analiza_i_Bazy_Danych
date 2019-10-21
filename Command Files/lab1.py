934e7b3a59aa4200a7cd04e0712cb4501f31cbd4
rafb10 <38700521+rafb10@users.noreply.github.com>
2019-10-21 09:02:54 +0200

    Update lab1.py

--- Command Files/lab1.py
+++ Command Files/lab1.py
@@ -6,12 +6,9 @@ Created on Mon Oct 14 11:36:38 2019
 """
 import pandas as pd
 
-data = pd.read_excel("dem_score.xlsx", index = None)
+#data = pd.read_csv("dem_score.csv");
+data = pd.read_excel("dem_score.xlsx")
 data = data.transpose()
 
-#data = pd.read_csv("dem_score.csv", index = None)
-#data = data.transpose()
-
-print(data)
-
 data.to_csv("final_data.csv")
+#df_new = pd.read_csv('final_data.csv').drop(['unnamed 0'],axis=1)
